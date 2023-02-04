b943a4251cc96ca5b368f950077e655a41f7adf6
Kroumy <alexandre.joignant@gmail.com>
2023-02-04 09:52:30 +0100

    add error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,5 +1,15 @@
+import errorLogo from "../../assets/404.svg"
+import './Error.css'
+import { Link } from "react-router-dom"
+
 function Error() {
-  return <h1>Error</h1>
+  return (
+    <main className="errorContainer">
+      <img src={errorLogo} alt="logo 404" className="errorLogo"/>
+      <span className="notFoundText">Oups! La page que vous demandez n'existe pas.</span>
+      <Link to="/" className="homeLink">Retourner sur la page d'accueil</Link>
+    </main>
+  )
 }
 
 export default Error
